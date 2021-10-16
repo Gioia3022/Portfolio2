389bb8f10217ecc4fbb2ad4739e5e162358997f5
Gioia3022 <gioiagaliazzo04@gmail.com>
2021-10-16 18:19:13 +0200

    Graph class implemented Txt file read in class graph None of the methodes are called testing must be made

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Graph g= new Graph();
+
     }
 }
